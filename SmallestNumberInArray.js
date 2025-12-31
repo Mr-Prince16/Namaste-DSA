f2071a22d87fccf0293eb1d86ea601a729d45111
Srinjoy Bhattacharya <srinjoy.bhattacharya@huly.ai>
2025-12-31 12:08:01 +0530

    normal practice

--- SmallestNumberInArray.js
+++ SmallestNumberInArray.js
@@ -1,11 +1,12 @@
-function SmallestNumberInArray(arr) {
+function smallestNumberinArray(arr){
     let min = Infinity;
-    for (let i = 0; i < arr.length; i++){
-        if (arr[i] <  min) {
-            min = arr[i]
+    for (let i = 0; i< arr.length; i++){
+        if(arr[i] < min){
+            min = arr[i];
         }
     }
     return min;
 }
 
-console.log(SmallestNumberInArray([12,3,9,10]))
+let array = [-1,-3,10,19,15]
+console.log(smallestNumberinArray(array));
